0f6ce290640c1ec38a4442f663613c657362ad80
Stefan Sava <Stefan.Sava@endava.com>
2025-10-15 15:18:11 +0300

    feat: added ch45 ex6

---
+++ src/ch45/ex6/Triangle.java
@@ -0,0 +1,12 @@
+package ch45.ex6;
+
+public class Rectangle extends Shape {
+    @Override
+    double area() {
+        return 10;
+    }
+    @Override
+    double perimeter() {
+        return 20;
+    }
+}
